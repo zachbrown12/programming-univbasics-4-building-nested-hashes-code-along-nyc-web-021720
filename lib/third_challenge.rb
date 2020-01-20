6b11b71e2f2a79a7371e08981f38fc82c418c457
Zach Brown <zachmb22@gmail.com>
2020-01-20 23:12:23 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,30 +1,18 @@
 def third_challenge
-  epic_tragedy = {
+ epic_tragedy = {
    :montague => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :hero => {
-        #your key/value pairs here
-        },
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
       :hero_friends => []
-   }, 
+   },
    :capulet => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :heroine => {
-        #your key/value pairs here
-        },
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
       :heroine_friends => []
    }
-  }
+}
 
   
 end
